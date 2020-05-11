cf6b191c73622c1fa381d39396688dfb6c5e6e1f
Movle <richard.xiu.520@gmail.com>
2020-05-11 14:42:30 +0800

    Scala之IO文件操作

---
+++ src/main/scala/Array.scala
@@ -0,0 +1,37 @@
+import scala.collection.mutable.ArrayBuffer
+
+/**
+ * @ClassName Array
+ * @MethodDesc: Scala之数组
+ * @Author Movle
+ * @Date 5/11/20 2:05 下午
+ * @Version 1.0
+ * @Email movle_xjk@foxmail.com
+ **/
+object Array {
+
+  def main(args: Array[String]): Unit = {
+    //数组创建方式1
+    val a = new Array[Int](10)
+    //遍历数组
+    for(s<-a)println(s)
+
+    val b = new Array[String](5)
+    for(s<-b)println(s)
+    //var c = Array("Tom","Jack","May")
+    //for(s<-c)println(s)
+    //创建一个变长数组
+    val d = ArrayBuffer[Int]()
+    d+=1
+    d+=2
+    d+=3
+    d+=(10,20,30)
+    //变长数组转换为普通数组
+    d.toArray
+    //遍历数组
+    d.foreach(println)
+
+
+
+  }
+}
--- src/main/scala/ScalaIO.scala
+++ src/main/scala/ScalaIO.scala
@@ -1,3 +1,7 @@
+import java.io.{File, FileInputStream, PrintWriter}
+
+import scala.io.Source.fromFile
+import scala.io.Source.fromURL
 /**
  * @ClassName LazyObject
  * @MethodDesc: ScalaIO
@@ -9,9 +13,42 @@
 object ScalaIO {
 
   def main(args: Array[String]): Unit = {
-    var words = scala.io.Source.fromFile("/users/macbook/TestInfo/student.txt").mkString
 
-    println(words)
+    val source1 = fromFile("/users/macbook/TestInfo/student.txt")
+
+    //读取整个文件
+    println("--------mkString----------")
+    println(source1.mkString)
+
+    val source2=fromFile("/users/macbook/TestInfo/student.txt")
+    //按行读取
+    println("----------lines----------------")
+    val lines = source2.getLines()
+    lines.foreach(println)
+
+    //读取二进制文件
+    println("--------Read Bytes---------")
+    var file = new File("/users/macbook/TestInfo/hudson.war")
+    //构建一个inputstream
+    var in = new FileInputStream(file)
+    //构造buffer
+    var buffer = new Array[Byte](file.length().toInt)
+    //读取
+    in.read(buffer)
+    println(buffer.length)
+    //关闭
+    in.close()
+
+    //从URL中获取信息
+    println("--------fromURL---------")
+    var source3 = fromURL("https://www.baidu.com","UTF-8")
+    println(source3.mkString)
+
+    //写入文件
+    println("--------Write File---------")
+    var out = new PrintWriter("/users/macbook/TestInfo/insert_0601.txt")
+    for (i <- 0 until 10) out.println(i)
+    out.close()
   }
 
 }
