a5c68b3c93714ce49cecd8e2e918322d6f5a042d
Movle <richard.xiu.520@gmail.com>
2020-05-11 11:30:08 +0800

    Scala之for循环的四种写法

---
+++ src/main/scala/For4.scala
@@ -0,0 +1,35 @@
+/**
+ * @ClassName For4
+ * @MethodDesc: for循环的四种写法
+ * @Author Movle
+ * @Date 5/11/20 10:42 上午
+ * @Version 1.0
+ * @Email movle_xjk@foxmail.com
+ **/
+object For4 {
+
+  def main(args: Array[String]): Unit = {
+
+    var list = List("LiMing","May","Mike")
+
+    println("for循环的第一种写法")
+    for(s<-list) println(s)
+
+    println("for循环的第二种写法")
+    for{
+      s <-list
+      if(s.length >3)
+    }println(s)
+
+    println("for循环的第三种写法")
+    for(s <- list if s.length < 3) println(s)
+
+    println("for循环的第四种写法")
+    var newList = for{
+      s <- list
+      s1=s.toUpperCase()
+    }yield(s1)
+    for(s1<-newList) println(s1)
+
+  }
+}
---
+++ src/main/scala/WhileCircle.scala
@@ -0,0 +1,11 @@
+/**
+ * @ClassName WhileCircle
+ * @MethodDesc: TODO WhileCircle功能介绍
+ * @Author Movle
+ * @Date 5/11/20 11:05 上午
+ * @Version 1.0
+ * @Email movle_xjk@foxmail.com
+ **/
+object WhileCircle {
+
+}
