fb7bef5b8cb1beb0a8dd97fdebb25c2f253bc115
Movle <richard.xiu.520@gmail.com>
2020-05-11 13:51:05 +0800

    Scala之while循环，dowhile循环，foreach迭代

--- src/main/scala/WhileCircle.scala
+++ src/main/scala/WhileCircle.scala
@@ -1,6 +1,6 @@
 /**
  * @ClassName WhileCircle
- * @MethodDesc: TODO WhileCircle功能介绍
+ * @MethodDesc: while循环与dowhile循环还有foreach迭代
  * @Author Movle
  * @Date 5/11/20 11:05 上午
  * @Version 1.0
@@ -8,6 +8,30 @@
  **/
 object WhileCircle {
 
+  /**
+   * while循环与dowhile循环还有foreach迭代
+   * @param args
+   */
+  def main(args: Array[String]): Unit = {
+    var list = List("LiMing","May","Mike")
+
+    var i=0
+    println("******while循环******")
+    while(i<list.length){
+      println(list(i))
+      i+=1
+    }
+
+    println("******dowhile循环******")
+    var j=0
+    do{
+      println(list(j))
+      j+=1
+    }while(j<list.length)
+
+    println("******foreach******")
+    list.foreach(println)
+  }
 
 }
 
