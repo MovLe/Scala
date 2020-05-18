5687d6326955c7aa6419a8f9fcd3767ef4c87fcd
Movle <richard.xiu.520@gmail.com>
2020-05-18 23:50:56 +0800

    Spark Streaming 实战：窗口操作，每10秒，把过去30秒的数据取出来

---
+++ src/main/scala/spark/streamingshizhan/MyNetWorkWordCountByWindow.scala
@@ -0,0 +1,47 @@
+package spark.streamingshizhan
+
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.Seconds
+import org.apache.spark.storage.StorageLevel
+/**
+ * @ClassName MyNetWorkWordCountByWindow
+ * @MethodDesc: TODO MyNetWorkWordCountByWindow功能介绍
+ * @Author Movle
+ * @Date 5/18/20 11:49 下午
+ * @Version 1.0
+ * @Email movle_xjk@foxmail.com
+ **/
+object MyNetWorkWordCountByWindow {
+
+  def main(args: Array[String]): Unit = {
+    System.setProperty("hadoop.home.dir", "/Users/macbook/Documents/hadoop/hadoop-2.8.4")
+    Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
+    Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
+
+    val conf = new SparkConf().setAppName("MyNetWorkWordCountByWindow").setMaster("local[2]")
+
+    val ssc = new StreamingContext(conf,Seconds(1))
+
+    val lines = ssc.socketTextStream("192.168.1.121", 1235, StorageLevel.MEMORY_ONLY)
+
+    val words = lines.flatMap(_.split(" ")).map((_,1))
+
+    /**
+     * reduce By Key And Window
+     * 三个参数
+     * 1、要进行什么操作
+     * 2、窗口的大小
+     * 3、窗口滑动的距离
+     */
+
+    val result = words.reduceByKeyAndWindow((x:Int,y:Int)=>(x+y),Seconds(30),Seconds(10))
+
+    result.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
